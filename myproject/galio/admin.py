caa9b692c10656c2b435e94b4f38eb7ad0dd63c4
Queency96 <queency96@gmail.com>
2024-12-25 11:26:00 +0100

    added Model for Products, Categories and Subcategories | created superuser with Name: admin, Pwd: 1234

--- myproject/galio/admin.py
+++ myproject/galio/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ['name', 'description']
+
+@admin.register(models.SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+  list_display = ['name', 'category', 'description']
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ['name', 'description', 'price', 'stock', 'category', 'subcategory', 'is_available']
+  
+  
