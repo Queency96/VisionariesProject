2ce746662c6aad22337c70982032dafe2e2d6562
Queency96 <queency96@gmail.com>
2025-01-19 17:41:53 +0100

    Updated all files

--- myproject/galio/admin.py
+++ myproject/galio/admin.py
@@ -30,16 +30,4 @@ class SubCategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
   list_display = ['name', 'description', 'price', 'stock', 'category', 'subcategory', 'is_available']
 
-@admin.register(models.Category)
-class CategoryAdmin(admin.ModelAdmin):
-  list_display = ['name', 'description']
-
-@admin.register(models.SubCategory)
-class SubCategoryAdmin(admin.ModelAdmin):
-  list_display = ['name', 'category', 'description']
-
-@admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin):
-  list_display = ['name', 'description', 'price', 'stock', 'category', 'subcategory', 'is_available']
-  
   
